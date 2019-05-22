ccbb2bde0326e946dc3d95f980d181767ad48457
glazersh <38733528+glazersh@users.noreply.github.com>
2019-05-22 19:18:16 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var DButilsAzure = require('./DButils');
+
+var port = 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
+
+const usersRoutes = require('./routes/users');
+
+
+app.use('/users', usersRoutes);
+
+
+
+
+
