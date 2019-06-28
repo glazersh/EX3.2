4564b45f2b80a3f09a91c63d05c1cb5784169247
דור לבנטר <dorlev@auth.ad.bgu.ac.il>
2019-06-28 18:32:50 +0300

    click

--- routes/points.js
+++ routes/points.js
@@ -9,79 +9,92 @@ router.use(express.json());
 
 
 // 1
-router.get('/ListOfPoints', function(req, res){
+router.get('/ListOfPoints', function (req, res) {
     DButilsAzure.execQuery("SELECT * FROM Poi")
 
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
 
 // 2
-router.get('/ListOfPoints/:poiName', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM Poi WHERE poiName = '"+req.params.poiName +"'")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+router.get('/ListOfPoints/:poiName', function (req, res) {
+    DButilsAzure.execQuery("SELECT * FROM Poi WHERE poiName = '" + req.params.poiName + "'")
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
 
 //7
-router.get('/getRandomPOI', function(req, res){
-    DButilsAzure.execQuery("SELECT TOP 3 * FROM Poi WHERE poiRate > 3 " )
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+router.get('/getRandomPOI', function (req, res) {
+    DButilsAzure.execQuery("SELECT TOP 3 * FROM Poi WHERE poiRate > 3 ")
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
 //8
-router.get('/getLastReview', function(req, res){
-    DButilsAzure.execQuery("select Top 2 timeStemp from PoiReview where poiName='" + req.body.poiName+"'"+
-    "order by timeStemp DESC ")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+router.get('/getLastReview', function (req, res) {
+    DButilsAzure.execQuery("select Top 2 timeStemp from PoiReview where poiName='" + req.body.poiName + "'" +
+        "order by timeStemp DESC ")
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
 //8
-router.get('/getCategories', function(req, res){
+router.get('/getCategories', function (req, res) {
     DButilsAzure.execQuery("select categoryName from Categories")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
 //12
-router.get('/ListOfPointsByCategory/:cn', function(req, res){
-    DButilsAzure.execQuery("select * from Poi where categoryName = '" + req.params.cn +"'")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+router.get('/ListOfPointsByCategory/:cn', function (req, res) {
+    DButilsAzure.execQuery("select * from Poi where categoryName = '" + req.params.cn + "'")
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
+})
+
+// update watch
+router.put('/addPoiWatching', function (req, res) {
+    console.log("GHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+    DButilsAzure.execQuery("UPDATE Poi SET poiWatching = poiWatching + 1 WHERE poiName = '" + req.body.poiName + "'")
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 })
 
 
